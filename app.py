35d681333b3854ddfef357f178232f55e9324fc3
oliverfelix <jayandhans484@gmail.com>
2024-03-14 18:01:18 +0530

    data ingestion added

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
-from signLanguage.logger import logging
-from signLanguage.exception import SignException
-import sys
+from signLanguage.pipeline.training_pipeline import TrainPipeline
 
-#logging.info("welcome to the project")
-
-try:
-    a=7/'9'
-    
-except Exception as e:
-    raise SignException(e,sys)
+obj = TrainPipeline()
+obj.run_pipeline()
